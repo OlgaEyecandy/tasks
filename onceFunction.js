17b0bcc80a4fc47c62bda205bd8c72fe3cbf1acf
Olga <39336446+OlgaEyecandy@users.noreply.github.com>
2018-07-31 14:24:12 +0300

    Update onceFunction.js

--- onceFunction.js
+++ onceFunction.js
@@ -6,25 +6,16 @@
 function inc(a) {
     return a + 1;
 }
-
 //res1 is 43
 var res2 = incOnce(77); //res2 is 43, NOT 78
 
 Требования:
-
 1. Не допустимы повторные вызовы функции-аргумента
 2. Функция once должна корректно работать с функциями нескольких аргументов
 3. Функция once должна корректно работать с методами
 4. Функция once должна корректно работать с функциями, которые кидают исключения. 
 В таких случаях, новая функция должна выбрасывать одно и то же ислючение при всех последующих вызовах.
-
 */
-
-//нужно иметь возможность сделать так:
-//используем bind, а потом
-
-
-
 var once = f => {
     var result;
     var isCalled = false;
@@ -34,36 +25,69 @@ var once = f => {
             try {
                 result = f.apply(this, rest);
             } catch (e) {
-                result = new Error(`Во время выполнения функции произошла ошибка: ${e.message}`);
+                result = new Error(`An error occured while executing the function: ${e.message}`);
             }
-        } 
-        if (result instanceof Error){
+        }
+        if (result instanceof Error) {
             throw result;
-        } 
+        }
+
         return result;
+
     }
 }
 var inc = a => a + 1;
-//var incOnce = once(inc);
-var incOnce = once(undefined);
-try {
-   var res1 = incOnce(42); 
-
-}
-catch(e){
-    console.log("text" + e)
-}
-try {
-    var res1 = incOnce(46); 
- 
- }
- catch(e){
-     console.log("text" + e)
- }
-/* console.log(incOnce(2))
+var incOnce = once(inc);
 var res1 = incOnce(42);
-console.log(res1);
-var res2 = incOnce(77);
-console.log(res2);
-var res3 = incOnce(79);
-console.log(res3); */
+describe("Once", function () {
+    describe("Тест на различные аргументы", function () {
+
+        it("Функция once корректно принимает один аргумент", function () {
+            incOnce = once(inc);
+            assert.deepEqual(incOnce(42), 43);
+            assert.deepEqual(incOnce(42), 43);
+        });
+
+        it("Функция once корректно принимает несколько аргументов", function () {
+            var sum = (a, b) => a + b;
+            var sumOnce = once(sum);
+            assert.deepEqual(sumOnce(42, 53), 95);
+            assert.deepEqual(sumOnce(7, 5), 95);
+        });
+        it("Функция once корректно работает с методами", function () {
+            var obj = {
+                i: 0,
+                inc: function () {
+                    this.i += 1;
+                    return this.i;
+                }
+            };
+            incOnce = once(obj.inc).bind(obj);
+            assert.deepEqual(incOnce(1), 1);
+            assert.deepEqual(incOnce(5), 1);
+        });
+
+    });
+
+    describe("Тест на ошибки", function () {
+
+        it("Функция once корректно отображает ошибку", function () {
+            inc = a => a + 1;
+            var newErr = () => {
+                var called = false;
+                return () => {
+                    if (!called) {
+                        called = true;
+                        throw new Error(`test2`);
+                    };
+                    throw new Error('test1');
+                }
+            }
+
+            incOnce = once(newErr());
+            assert.throw(incOnce, Error, `test2`);
+            assert.throw(incOnce, Error, `test2`);
+        });
+
+    });
+});
